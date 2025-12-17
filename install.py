a1264e50a3cd08fc6b4bcc7032abd754eff99706
TecXCTO <cto.tecx@gmail.com>
2025-12-17 14:49:45 +0530

    Update install.py

--- install.py
+++ install.py
@@ -22,7 +22,7 @@ with open('requirements_all.txt', 'r') as f:
                 print(f"Failed to install {package}. Skipping...")
                 un_installed_list.extend(package)
 with open('installed_list.txt', 'w') as f:
-    f.writelines([i + '\n' for i in installed_list])
+    f.writelines([j + '\n' for i,j in installed_list])
 with open('un_installed_list.txt', 'w') as f:
-    f.writelines([i + '\n' for i in un_installed_list])
+    f.writelines([j + '\n' for i,j in un_installed_list])
 print(f"Installed Python Packages List: {installed_list}/n Uninstalled Python Packages List: {un_installed_list}/n ")
