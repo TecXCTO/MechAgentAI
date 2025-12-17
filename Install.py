80fd28086194c5b22cc711d48041a9f2db9e4015
TecXCTO <cto.tecx@gmail.com>
2025-12-17 12:16:50 +0530

    Update Install.py

--- Install.py
+++ Install.py
@@ -1,5 +1,6 @@
 import subprocess
-
+installed_list=
+un_installed_list=
 with open('requirements.txt', 'r') as f:
     for line in f:
         package = line.strip()
@@ -9,3 +10,15 @@ with open('requirements.txt', 'r') as f:
             except subprocess.CalledProcessError:
                 print(f"Failed to install {package}. Skipping...")
               
+with open('requirements_full.txt', 'r') as f:
+    for line in f:
+        package = line.strip()
+        if package and not package.startswith('#'):
+            try:
+                subprocess.check_call(['pip', 'install', package])
+                installed_list=package
+            except subprocess.CalledProcessError:
+                print(f"Failed to install {package}. Skipping...")
+                un_installed_list=package
+
+print(f"Installed Python Packages List: {installed_list}/n Uninstalled Python Packages List: {un_installed_list}/n ")
