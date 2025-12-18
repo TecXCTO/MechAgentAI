34f09ffacbe925369d6ed9fd244405c5436d0591
TecXCTO <cto.tecx@gmail.com>
2025-12-18 08:20:48 +0530

    Update custom_functions.py

--- mech_agent_ai_model/custom_functions.py
+++ mech_agent_ai_model/custom_functions.py
@@ -5,11 +5,11 @@ def get_fx_rate(base: str, target: str) -> dict:
     return {"rate": 10.0}
 
 # In agent.py
-from .custom_functions import get_fx_rate  # Relative import
-from google.adk.agents.llm_agent import Agent
+##from .custom_functions import get_fx_rate  # Relative import
+##from google.adk.agents.llm_agent import Agent
 
-root_agent = Agent(
+##root_agent = Agent(
     # ... other parameters ...
-    tools=[get_fx_rate],
-)
+    ##tools=[get_fx_rate],
+##)
 
